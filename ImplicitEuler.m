f0308e32a5429c65d0cd289aaf96cfa4c5e79b60
EgorKudryashov <70726535+EgorKudryashov@users.noreply.github.com>
2022-03-29 17:15:35 +0300

    Неявные методы Эйлера и средней точки. 1-этаптные методы

---
+++ ImplicitEuler.m
@@ -0,0 +1,27 @@
+function [X,Y] = ImplicitEuler(F, x_0, y_0, h, RightBorder)
+    
+% Неявный метод Эйлера
+%     A = 1;
+%     B = 1;
+%     C = 1;
+    
+    v = 0; X = [];
+    v = v + 1;
+    X(v) = (x_0);
+    Y = y_0;
+    
+    while (x_0 < RightBorder)
+        x_1 = x_0 + h;
+        %f = @(y) h*F(x_1,y) + y_0 - y;
+        f = @(k) k - F(x_1,y_0+h*k);
+        k = fsolve(f, F(x_0,y_0));
+        y_1 = y_0 + h*k; 
+        
+        x_0 = x_1;
+        y_0 = y_1;
+        v = v + 1;
+        X(v) = x_0; 
+        Y = [Y, y_0];
+    end
+end
+
